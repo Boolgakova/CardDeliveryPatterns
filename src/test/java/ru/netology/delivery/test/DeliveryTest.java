44fc03b20751fb22b7368c91dfdf6fa7eca6cc0c
Boolgakova <boolgakova@gmail.com>
2022-09-01 16:32:08 +0300

    Allure intagrated

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -1,8 +1,8 @@
 package ru.netology.delivery.test;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import ru.netology.delivery.data.DataGenerator;
 
@@ -10,6 +10,16 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 class DeliveryTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
     @BeforeEach
     void setup() {
@@ -41,6 +51,34 @@ class DeliveryTest {
 
         $(".notification__content")
                 .shouldHave(text("Встреча успешно запланирована на " + secondMeetingDate))
+                .shouldBe(visible);
+    }
+
+    @Test
+    @DisplayName("Should failed")
+    void shouldFailed() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+        $("[data-test-id=city] input").setValue(validUser.getCity());
+        $("[data-test-id=date] input").doubleClick().sendKeys(firstMeetingDate);
+        $("[data-test-id=name] input").setValue(validUser.getName());
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
+        $("[data-test-id=agreement]").click();
+        $(By.className("button")).click();
+
+        $(".notification__content")
+                .shouldHave(text("Встреча успешно запланирована на " + firstMeetingDate))
+                .shouldBe(visible);
+
+        $("[data-test-id=date] input").doubleClick().sendKeys(secondMeetingDate);
+        $(By.className("button")).click();
+        $x("//span[contains(text(),'Перепланировать')]").click();
+
+        $(".notification__content")
+                .shouldHave(text("Встреча успешно перепланирована на " + secondMeetingDate))
                 .shouldBe(visible);
     }
 }
